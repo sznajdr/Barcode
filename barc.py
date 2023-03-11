315b227caefbb02b6867ba42a2de778533433bf3
sznajdr <46231329+sznajdr@users.noreply.github.com>
2023-03-11 11:58:49 +0100

    Update barc.py

--- barc.py
+++ barc.py
@@ -16,18 +16,13 @@ if not os.path.exists("Arial.ttf"):
         f.write(response.content)
 
 # Define widgets
-barcode_textbox = st.text_input("Barcode:")
-title_textbox = st.text_input("Title:")
+barcode_textbox = st.text_input("Barcode:", value=str(example_number))
+title_textbox = st.text_input("Title:", value=str(example_title))
 generate_button = st.button("Generate Barcode")
 
 # Define example number and title values
 example_number = 6938936708209
 example_title = "Druckbett PEI-Guss ROUGH 330x330"
-
-
-# Add input fields for the number and title
-number_input = st.text_input("Product Number", value=str(example_number))
-title_input = st.text_input("Product Title", value=example_title)
 
 # Define functions
 def generate_barcode():
